6dc4d5641523bc1088998b4e42d6ce3e4e472a9f
g33kt8tr <135957664+g33kt8tr@users.noreply.github.com>
2025-07-24 07:16:43 +0200

    Update mount-jenkins-nvme01.sh

--- mount-jenkins-nvme01.sh
+++ mount-jenkins-nvme01.sh
@@ -1,4 +1,4 @@
-DEVICE="/dev/sda3"  # Replace with your actual device or partition
+DEVICE="/dev/nvme0n1"  # Replace with your actual device or partition
 MOUNT_POINT="/mnt/data1/jenkins"
 FS_TYPE="ext4"
 
